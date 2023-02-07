2698141607b3c98df333df26beaa826c7b2024fd
Ranjith4024 <kumar.ranjith.srk@gmail.com>
2023-02-07 09:51:22 +0530

    adding

---
+++ main.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+const employees = [
+  { name: 'David Carlson', 
+     age: 30 
+   },
+  { name: 'John Cena', 
+     age: 34 
+    },
+  { name: 'Mike Sheridan', 
+     age: 25 
+   },
+  { name: 'John Carte', 
+    age: 50 
+   }
+];
+
+const john = employees.filter(function(emp){
+      return emp.age > 30
+	  })
+console.log(john);
